7384910d0ed4eb2bee45df7acc86aa4b43784209
Enrique Ortiz <hi@enriqueortiz.dev>
2021-12-19 23:19:31 +0100

    feat: add network response types

---
+++ packages/types/src/network-response-types.ts
@@ -0,0 +1,23 @@
+import { IChain } from './types'
+
+export type ChainsResponse = {
+  chains: IChain[]
+}
+
+export type NetworkSummaryResponse = {
+  appsStaked: number
+  nodesStaked: number
+  poktStaked: number
+}
+
+export type NetworkDailyRelayBucket = {
+  total_relays: number
+  bucket: string
+}
+
+export type NetworkDailyRelaysResponse = NetworkDailyRelayBucket[]
+
+export type NetworkWeeklyAggregatedRelaysResponse = {
+  successful_relays: number
+  total_relays: number
+}
