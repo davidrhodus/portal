00093364a5180eceafba898639c84f1e5614a865
Albin <albinesteban@outlook.com>
2022-03-25 09:47:36 -0400

    feat: add basic performance card

--- packages/frontend/src/screens/Dashboard/Network/NetworkStatus.tsx
+++ packages/frontend/src/screens/Dashboard/Network/NetworkStatus.tsx
@@ -389,6 +389,116 @@ export default function NetworkStatus() {
                 </Box>
                 <Spacer size={4 * GU} />
                 <LatestBlock data={latestBlockData} />
+                <Spacer size={4 * GU} />
+                <Card
+                  css={`
+                    padding: ${3 * GU}px;
+                  `}
+                >
+                  <React.Fragment>
+                    <h3
+                      css={`
+                        ${textStyle('title3')};
+                        margin-bottom: ${2 * GU}px;
+                      `}
+                    >
+                      Performance
+                    </h3>
+                    <h4
+                      css={`
+                        color: ${theme.accentAlternative};
+                        ${textStyle('body2')};
+                        font-weight: 700;
+                        margin-bottom: ${GU * 2}px;
+                      `}
+                    >
+                      Relays
+                    </h4>
+
+                    <div
+                      css={`
+                        display: flex;
+                        justify-content: space-between;
+                      `}
+                    >
+                      <div
+                        css={`
+                          display: flex;
+                          flex-direction: column;
+                        `}
+                      >
+                        <p
+                          css={`
+                            color: ${theme.placeholder};
+                            font-size: ${GU + 6}px;
+                            font-weight: 500;
+                          `}
+                        >
+                          Today:
+                        </p>
+                        <p
+                          css={`
+                            color: ${theme.inactive};
+                            font-size: ${GU + 6}px;
+                            font-weight: 700;
+                          `}
+                        >
+                          231.5M
+                        </p>
+                      </div>
+                      <div
+                        css={`
+                          display: flex;
+                          flex-direction: column;
+                        `}
+                      >
+                        <p
+                          css={`
+                            color: ${theme.placeholder};
+                            font-size: ${GU + 6}px;
+                            font-weight: 500;
+                          `}
+                        >
+                          Month:
+                        </p>
+                        <p
+                          css={`
+                            color: ${theme.inactive};
+                            font-size: ${GU + 6}px;
+                            font-weight: 700;
+                          `}
+                        >
+                          231.5M
+                        </p>
+                      </div>
+                      <div
+                        css={`
+                          display: flex;
+                          flex-direction: column;
+                        `}
+                      >
+                        <p
+                          css={`
+                            color: ${theme.placeholder};
+                            font-size: ${GU + 6}px;
+                            font-weight: 500;
+                          `}
+                        >
+                          Max:
+                        </p>
+                        <p
+                          css={`
+                            color: ${theme.inactive};
+                            font-size: ${GU + 6}px;
+                            font-weight: 700;
+                          `}
+                        >
+                          231.5M
+                        </p>
+                      </div>
+                    </div>
+                  </React.Fragment>
+                </Card>
                 {!compactMode && (
                   <>
                     <Spacer size={4 * GU} />
